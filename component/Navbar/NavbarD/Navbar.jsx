a35edec8afcded853eacce2293882ccc955c6e56
rutik <rutikkarlewar07@gmail.com>
2026-09-09 22:10:06 +0530

    Update navbar

--- component/Navbar/NavbarD/Navbar.jsx
+++ component/Navbar/NavbarD/Navbar.jsx
@@ -35,6 +35,7 @@ export default function Navbar() {
             color: "#0b1f3a",
             fontSize: "26px",
             fontWeight: 700,
+            whiteSpace: "nowrap",
           }}
         >
           InfoCore Solutions
@@ -45,23 +46,105 @@ export default function Navbar() {
           style={{
             display: "flex",
             alignItems: "center",
-            gap: "32px",
+            gap: "30px",
           }}
         >
-          <Link href="/" style={navLinkStyle}>
-            Home
-          </Link>
+          {/* Services */}
+          <div style={dropdownWrapperStyle}>
+            <Link href="/services" style={navLinkStyle}>
+              Services <span style={arrowStyle}>⌄</span>
+            </Link>
 
-          <Link href="/services" style={navLinkStyle}>
-            Services
+            <div style={dropdownStyle}>
+              <Link
+                href="/services/website-development"
+                style={dropdownLinkStyle}
+              >
+                Website Development
+              </Link>
+
+              <Link href="/services/app-development" style={dropdownLinkStyle}>
+                App Development
+              </Link>
+
+              <Link
+                href="/services/software-development"
+                style={dropdownLinkStyle}
+              >
+                Custom Software
+              </Link>
+
+              <Link href="/services/seo" style={dropdownLinkStyle}>
+                SEO
+              </Link>
+            </div>
+          </div>
+
+          {/* Tools */}
+          <div style={dropdownWrapperStyle}>
+            <Link href="/tools" style={navLinkStyle}>
+              Tools <span style={arrowStyle}>⌄</span>
+            </Link>
+
+            <div style={dropdownStyle}>
+              <Link href="/tools/email" style={dropdownLinkStyle}>
+                Email Tools
+              </Link>
+
+              <Link href="/tools/whatsapp" style={dropdownLinkStyle}>
+                WhatsApp Tools
+              </Link>
+
+              <Link href="/tools/messaging" style={dropdownLinkStyle}>
+                Messaging Tools
+              </Link>
+
+              <Link href="/tools/free" style={dropdownLinkStyle}>
+                Free Tools
+              </Link>
+
+              <Link href="/tools/paid" style={dropdownLinkStyle}>
+                Premium Tools
+              </Link>
+            </div>
+          </div>
+
+          {/* Solutions */}
+          <div style={dropdownWrapperStyle}>
+            <Link href="/solutions" style={navLinkStyle}>
+              Solutions <span style={arrowStyle}>⌄</span>
+            </Link>
+
+            <div style={dropdownStyle}>
+              <Link
+                href="/solutions/business-automation"
+                style={dropdownLinkStyle}
+              >
+                Business Automation
+              </Link>
+
+              <Link
+                href="/solutions/digital-transformation"
+                style={dropdownLinkStyle}
+              >
+                Digital Transformation
+              </Link>
+
+              <Link
+                href="/solutions/custom-solutions"
+                style={dropdownLinkStyle}
+              >
+                Custom Solutions
+              </Link>
+            </div>
+          </div>
+
+          <Link href="/industries" style={navLinkStyle}>
+            Industries
           </Link>
 
           <Link href="/about-us" style={navLinkStyle}>
             About Us
-          </Link>
-
-          <Link href="/industries" style={navLinkStyle}>
-            Industries
           </Link>
 
           <Link href="/contact" style={navLinkStyle}>
@@ -76,10 +159,11 @@ export default function Navbar() {
             background: "#0057d9",
             color: "#fff",
             textDecoration: "none",
-            padding: "14px 28px",
+            padding: "14px 26px",
             borderRadius: "4px",
             fontSize: "15px",
             fontWeight: 600,
+            whiteSpace: "nowrap",
           }}
         >
           Get a Free Consultation
@@ -94,4 +178,37 @@ const navLinkStyle = {
   textDecoration: "none",
   fontSize: "15px",
   fontWeight: 500,
+  whiteSpace: "nowrap",
+};
+
+const arrowStyle = {
+  fontSize: "17px",
+  marginLeft: "3px",
+  color: "#64748b",
+};
+
+const dropdownWrapperStyle = {
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+};
+
+const dropdownStyle = {
+  position: "absolute",
+  top: "38px",
+  left: "-15px",
+  minWidth: "220px",
+  background: "#fff",
+  border: "1px solid #e5e7eb",
+  borderRadius: "6px",
+  boxShadow: "0 10px 30px rgba(15, 23, 42, 0.08)",
+  padding: "8px 0",
+};
+
+const dropdownLinkStyle = {
+  display: "block",
+  padding: "10px 16px",
+  color: "#334155",
+  textDecoration: "none",
+  fontSize: "14px",
 };
